5c1544d77dd8509a4b33df5b2e52f69a9b184271
eapalacios57 <epalacios@qvision.us>
2022-11-21 09:23:09 -0500

    Ajustes file groovy

--- vars/status_weblogic.groovy
+++ vars/status_weblogic.groovy
@@ -11,7 +11,8 @@ def statusStage(WEBLOGIC_CREDENTIAL_USR, WEBLOGIC_CREDENTIAL_PSW, urlWl, cluster
     echo "if '' in lista:" >> statusServer.py
     echo " lista.remove('')" >> statusServer.py
     echo 'for i in lista:' >> statusServer.py
-    echo " cd('domainRuntime:/ServerLifeCycleRuntimes/' + i)" >> statusServer.py
+    echo 'domainRuntime()' >> statusServer.py
+    echo " cd('ServerLifeCycleRuntimes/' + i)" >> statusServer.py
     echo ' state = cmo.getState()' >> statusServer.py
     echo ' if(state != "RUNNING"):' >> statusServer.py
     echo '  disconnect()' >> statusServer.py
