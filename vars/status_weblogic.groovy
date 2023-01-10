3461d3981f8a0971875a85e06e681c048bc04275
epalacios@qvision.us-Desarrollador <48025620+eapalacios57@users.noreply.github.com>
2023-01-10 09:24:54 -0500

    Update status_weblogic.groovy

--- vars/status_weblogic.groovy
+++ vars/status_weblogic.groovy
@@ -12,7 +12,7 @@ def statusStage(WEBLOGIC_CREDENTIAL_USR, WEBLOGIC_CREDENTIAL_PSW, urlWl, ServerW
     echo 'for i in lista:' >> statusServer.py
     echo " cd('domainRuntime:/ServerLifeCycleRuntimes/' + i)" >> statusServer.py
     echo ' stateServer = cmo.getState()' >> statusServer.py
-    echo ' print " Status Server ------->>" + statusServer' >> statusServer.py
+    echo ' print " Status Server ------->>" + stateServer' >> statusServer.py
     echo ' cd("domainRuntime:/ServerRuntimes/" + i +"/ThreadPoolRuntime/ThreadPoolRuntime")' >> statusServer.py
     echo " detailServer = get('HealthState')" >> statusServer.py
     echo " healthServer = detailServer.toString().split(',')[2].split(':')[1].split('HEALTH_')[1]" >> statusServer.py
