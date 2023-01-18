b089b767e9451e08917baf43a2f5b089c970a4bd
epalacios@qvision.us-Desarrollador <48025620+eapalacios57@users.noreply.github.com>
2023-01-18 12:47:48 -0500

    Update status_weblogic.groovy

--- vars/status_weblogic.groovy
+++ vars/status_weblogic.groovy
@@ -1,12 +1,11 @@
-def statusStage(WEBLOGIC_CREDENTIAL_USR, WEBLOGIC_CREDENTIAL_PSW, urlWl, ServerWL1, ServerWL2=''){
+def statusStage(WEBLOGIC_CREDENTIAL_USR, WEBLOGIC_CREDENTIAL_PSW, urlWl, serverWl1, serverWl2=''){
     
     sh """
     touch statusServer.py
     echo 'connect("${WEBLOGIC_CREDENTIAL_USR}","${WEBLOGIC_CREDENTIAL_PSW}","${urlWl}")' >> statusServer.py
     echo 'lista = []' >> statusServer.py
-    echo "lista.append('${ServerWL1}')" >> statusServer.py
-    echo "lista.append('${ServerWL2}')" >> statusServer.py
-    echo 'print lista' >> statusServer.py
+    echo "lista.append('${serverWl1}')" >> statusServer.py
+    echo "lista.append('${serverWl2}')" >> statusServer.py
     echo "if '' in lista:" >> statusServer.py
     echo " lista.remove('')" >> statusServer.py
     echo 'for i in lista:' >> statusServer.py
@@ -18,10 +17,14 @@ def statusStage(WEBLOGIC_CREDENTIAL_USR, WEBLOGIC_CREDENTIAL_PSW, urlWl, ServerW
     echo ' cd("domainRuntime:/ServerRuntimes/" + i +"/ThreadPoolRuntime/ThreadPoolRuntime")' >> statusServer.py
     echo " detailServer = get('HealthState')" >> statusServer.py
     echo " healthServer = detailServer.toString().split(',')[2].split(':')[1].split('HEALTH_')[1]" >> statusServer.py
+    echo " arrayConfig = detailServer.toString().split(',')" >> statusServer.py
+    echo " for config in arrayConfig:" >> statusServer.py
+    echo "  if 'State' in i.split(':'):" >> statusServer.py
+    echo "    healtStatus = config.split(':')[1].split('HEALTH_')[1]" >> statusServer.py
     echo ' print "<<------------------------------------------------------------------------>>"' >> statusServer.py
-    echo ' print " Status Health Server " + i + " <<----------" + healthServer + "---------->>"' >> statusServer.py
+    echo ' print " Status Health Server " + i + " <<----------" + healtStatus + "---------->>"' >> statusServer.py
     echo ' print "<<------------------------------------------------------------------------>>"' >> statusServer.py
-    echo ' if(stateServer != "RUNNING" or healthServer != "OK"):' >> statusServer.py
+    echo ' if(stateServer != "RUNNING" or healtStatus != "OK"):' >> statusServer.py
     echo '  disconnect()' >> statusServer.py
     """
 }
